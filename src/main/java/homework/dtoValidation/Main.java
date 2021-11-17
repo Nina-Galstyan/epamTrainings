261a07b1c615f47ebae66611ec2f5fc474de0986
Admin <shukhyann@mail.ru>
2021-11-17 14:26:23 +0400

    initial commit

--- src/main/java/homework/dtoValidation/Main.java
+++ src/main/java/homework/dtoValidation/Main.java
@@ -6,7 +6,7 @@ import homework.dtoValidation.validator.Validator;
 
 import java.time.LocalDate;
 
-public class Mail {
+public class Main {
     public static void main(String[] args) throws IllegalAccessException {
         CustomerDto customerDto=new CustomerDto("A","annagmail.com", LocalDate.of(2021,5,11),120);
         Validator validator = new Validator();
